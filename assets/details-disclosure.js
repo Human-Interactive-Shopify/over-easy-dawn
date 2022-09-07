0300edf3b4682bc214ade0d7d3640af5c55024f6
Karlo Biscan <biscan.karlo@gmail.com>
2022-09-07 13:08:53 +0200

    Add FAQ to search

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -6,7 +6,7 @@ class DetailsDisclosure extends HTMLElement {
 
 		this.mainDetailsToggle.addEventListener('focusout', this.onFocusOut.bind(this));
 		this.mainDetailsToggle.addEventListener('toggle', this.onToggle.bind(this));
-		document.addEventListener('scroll', this.close.bind(this));
+		// document.addEventListener('scroll', this.close.bind(this));
 	}
 
 	onFocusOut() {
